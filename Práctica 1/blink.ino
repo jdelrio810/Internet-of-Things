236b585629c90b4769dec47ad91bba0e90ea0a7b
iotunipana <54504671+iotunipana@users.noreply.github.com>
2019-08-25 10:18:50 -0500

    Create blink.ino

---
+++ Práctica 1/blink.ino
@@ -0,0 +1,17 @@
+#include <ESP8266WiFi.h>
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(PIN_GPIO, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  
+  digitalWrite(PIN_GPIO, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(PIN_GPIO, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);    // wait for a second
+
+}
