211a44c26c0c216cf60cc6bd5ce52aea6f82eb56
iotunipana <54504671+iotunipana@users.noreply.github.com>
2019-09-15 17:47:03 -0500

    Update potenciometro.ino

--- Practica 3/potenciometro.ino
+++ Practica 3/potenciometro.ino
@@ -1,16 +1,19 @@
 int sensorPin = A0;    
 float sensorValue = 0;  
+int led = 5; //pin D1 
 
 void setup() {
 
   pinMode(sensorPin,INPUT);
+  pinMode(led,OUTPUT);
   Serial.begin(115200);
  
 }
 
 void loop() {
-
+  
   sensorValue = (3.3*analogRead(sensorPin))/1023;
+  analogWrite(led,analogRead(sensorPin));
   Serial.println(sensorValue);
   delay(1000);
   
