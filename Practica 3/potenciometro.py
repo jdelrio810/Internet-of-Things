647b6940316bbd071322d9bb13a091c61bcad98a
iotunipana <54504671+iotunipana@users.noreply.github.com>
2019-09-15 14:59:28 -0500

    Create potenciometro.py

---
+++ Practica 3/potenciometro.py
@@ -0,0 +1,21 @@
+import serial
+import requests
+
+ser = serial.Serial('/dev/"nombre_dispositivo"', 115200)
+
+# loop until manually stopped
+# first flush possibly existing data in the input buffer:
+
+ser.flushInput()
+
+while True:
+    try:
+        # read a single line from the serial interface represented by the ser object
+        lineBytes = ser.readline()
+        # convert Bytes returned by the ser.readline() function to String
+        line = lineBytes.decode('utf-8')
+        # print the read line to the output
+        print(line)
+    
+    except KeyboardInterrupt:
+        break # stop the while loop
