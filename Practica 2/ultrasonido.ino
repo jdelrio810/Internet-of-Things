66937d9b5d9bda0b062e300f37c03e017fcdf330
iotunipana <54504671+iotunipana@users.noreply.github.com>
2019-11-17 18:54:24 -0500

    Update ultrasonido.ino

--- Practica 2/ultrasonido.ino
+++ Practica 2/ultrasonido.ino
@@ -1,29 +1,30 @@
-const int Trigger = 2;   //Pin digital 2 para el Trigger del sensor
-const int Echo = 3;   //Pin digital 3 para el Echo del sensor
+int TRIG = 5;      // trigger en pin 10
+int ECO = 4;      // echo en pin 9
+int LED = 2;      // LED en pin 3
+int DURACION;
+int DISTANCIA;
 
-void setup() {
-  Serial.begin(9600);//iniciailzamos la comunicación
-  pinMode(Trigger, OUTPUT); //pin como salida
-  pinMode(Echo, INPUT);  //pin como entrada
-  digitalWrite(Trigger, LOW);//Inicializamos el pin con 0
+void setup()
+{
+  pinMode(TRIG, OUTPUT);  // trigger como salida
+  pinMode(ECO, INPUT);    // echo como entrada
+  pinMode(LED, OUTPUT);   // LED como salida
+  Serial.begin(115200);     // inicializacion de comunicacion serial a 9600 bps
+
 }
 
 void loop()
 {
-
-  long t; //timepo que demora en llegar el eco
-  long d; //distancia en centimetros
-
-  digitalWrite(Trigger, HIGH);
-  delayMicroseconds(10);          //Enviamos un pulso de 10us
-  digitalWrite(Trigger, LOW);
   
-  t = pulseIn(Echo, HIGH); //obtenemos el ancho del pulso
-  d = t/59;             //escalamos el tiempo a una distancia en cm
+  digitalWrite(TRIG, HIGH);     // generacion del pulso a enviar
+  delay(1);       // al pin conectado al trigger
+  digitalWrite(TRIG, LOW);    // del sensor
   
-  Serial.print("Distancia: ");
-  Serial.print(d);      //Enviamos serialmente el valor de la distancia
-  Serial.print("cm");
-  Serial.println();
-  delay(100);          //Hacemos una pausa de 100ms
+  DURACION = pulseIn(ECO, HIGH);  // con funcion pulseIn se espera un pulso
+            // alto en Echo
+        
+  DISTANCIA = DURACION / 58.2;    // distancia medida en centimetros
+  //Serial.println(DISTANCIA);    // envio de valor de distancia por monitor serial
+  delay(200);       // demora entre datos
+
 }
